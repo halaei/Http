9a05b41655543e6fd8303a72240ddd56e856a8ed
Payam Naderi <naderi.payam@gmail.com>
2015-05-27 16:40:33 +0430

    Init Response Message

---
+++ Message/HttpPhpResponse.php
@@ -0,0 +1,12 @@
+<?php
+namespace Poirot\Http\Message\Response;
+
+use Poirot\Http\Message\HttpResponse;
+
+class HttpPhpResponse extends HttpResponse
+{
+    function send()
+    {
+
+    }
+}
---
+++ Message/HttpResponse.php
@@ -0,0 +1,189 @@
+<?php
+namespace Poirot\Http\Message;
+
+use Poirot\Http\Header\HeaderFactory;
+use Poirot\Http\Interfaces\Message\iHttpResponse;
+
+class HttpResponse extends AbstractHttpMessage
+    implements iHttpResponse
+{
+    protected $statCode;
+    protected $statReason;
+    /**
+     * Map of standard HTTP status code/reason phrases
+     *
+     * @var array
+     */
+    protected $phrases = [
+        // INFORMATIONAL CODES
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        102 => 'Processing',
+        // SUCCESS CODES
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        207 => 'Multi-status',
+        208 => 'Already Reported',
+        // REDIRECTION CODES
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Found',
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        306 => 'Switch Proxy', // Deprecated
+        307 => 'Temporary Redirect',
+        // CLIENT ERROR
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Time-out',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Request Entity Too Large',
+        414 => 'Request-URI Too Large',
+        415 => 'Unsupported Media Type',
+        416 => 'Requested range not satisfiable',
+        417 => 'Expectation Failed',
+        418 => 'I\'m a teapot',
+        422 => 'Unprocessable Entity',
+        423 => 'Locked',
+        424 => 'Failed Dependency',
+        425 => 'Unordered Collection',
+        426 => 'Upgrade Required',
+        428 => 'Precondition Required',
+        429 => 'Too Many Requests',
+        431 => 'Request Header Fields Too Large',
+        // SERVER ERROR
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Time-out',
+        505 => 'HTTP Version not supported',
+        506 => 'Variant Also Negotiates',
+        507 => 'Insufficient Storage',
+        508 => 'Loop Detected',
+        511 => 'Network Authentication Required',
+    ];
+
+    /**
+     * Set Options From Http Message String
+     *
+     * @param string $message Message String
+     *
+     * @throws \InvalidArgumentException
+     * @return $this
+     */
+    function fromString($message)
+    {
+        if (!preg_match_all('/.*[\n]?/', $message, $lines))
+            throw new \InvalidArgumentException('Error Parsing Request Message.');
+
+        $lines = $lines[0];
+
+        $firstLine = array_shift($lines);
+
+        $regex   = '/^HTTP\/(?P<version>1\.[01]) (?P<status>\d{3})(?:[ ]+(?P<reason>.*))?$/';
+        $matches = array();
+        if (!preg_match($regex, $firstLine, $matches))
+            throw new \InvalidArgumentException(
+                'A valid response status line was not found in the provided string'
+            );
+
+        $this->setVersion($matches['version']);
+        $this->setStatCode($matches['status']);
+        $this->setStatReason((isset($matches['reason']) ? $matches['reason'] : ''));
+
+        // headers:
+        while ($nextLine = array_shift($lines)) {
+            if ($nextLine == '')
+                // headers end
+                continue;
+
+            $this->getHeaders()->attach(HeaderFactory::fromString($nextLine));
+        }
+
+        // body:
+        $this->setBody(implode("\r\n", $lines));
+
+        return $this;
+    }
+
+    /**
+     * Set Response Status Code
+     *
+     * @param int $status
+     *
+     * @return $this
+     */
+    function setStatCode($status)
+    {
+        if (! is_numeric($status)
+            || is_float($status)
+            || $status < 100
+            || $status >= 600
+        )
+            throw new \InvalidArgumentException(sprintf(
+                'Invalid status code "%s"; must be an integer between 100 and 599, inclusive',
+                (is_scalar($status) ? $status : gettype($status))
+            ));
+
+        $this->statCode = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get Response Status Code
+     *
+     * @return int
+     */
+    function getStatCode()
+    {
+        return $this->statCode;
+    }
+
+    /**
+     * Set Status Code Reason
+     *
+     * @param string $reason
+     *
+     * @return $this
+     */
+    function setStatReason($reason)
+    {
+        $this->statReason = (string) $reason;
+
+        return $this;
+    }
+
+    /**
+     * Get Status Code Reason
+     *
+     * @return string
+     */
+    function getStatReason()
+    {
+        if (!$this->statReason)
+            ($code = $this->getStatCode() === null) ?: (
+                (!isset($this->phrases[$code])) ?: (
+                    $this->setStatReason($this->phrases[$code])
+                )
+            );
+        
+        return $this->statReason;
+    }
+}
