9b4534b43c227667a8b88d2d0738018568eb99ab
Payam Naderi <naderi.payam@gmail.com>
2015-11-19 21:30:43 +0330

    code cleanup

--- Header/HeaderPluginsManager.php
+++ Header/HeaderPluginsManager.php
@@ -2,38 +2,11 @@
 namespace Poirot\Http\Header;
 
 use Poirot\Container\Exception\ContainerInvalidPluginException;
-use Poirot\Container\Interfaces\iContainerBuilder;
 use Poirot\Container\Plugins\AbstractPlugins;
-use Poirot\Http\Interfaces\Message\iHttpMessage;
+use Poirot\Http\Interfaces\iHeader;
 
 class HeaderPluginsManager extends AbstractPlugins
 {
-    /**
-     * @var iHttpMessage
-     */
-    protected $_mess_object;
-
-    /**
-     * @override
-     *
-     * Construct
-     *
-     * @param iContainerBuilder $cBuilder
-     *
-     * @throws \Exception
-     */
-    function __construct(iContainerBuilder $cBuilder = null)
-    {
-        parent::__construct($cBuilder);
-
-        // Add Initializer To Inject Http Message Instance:
-        $thisContainer = $this;
-        $this->initializer()->addMethod(function() use ($thisContainer) {
-            // Inject Service Container Inside
-            $this->setMessageObject($thisContainer->getMessageObject());
-        }, 10000);
-    }
-
     /**
      * Validate Plugin Instance Object
      *
@@ -44,7 +17,7 @@ class HeaderPluginsManager extends AbstractPlugins
      */
     function validatePlugin($pluginInstance)
     {
-        if (!$pluginInstance instanceof iHttpPlugin)
+        if (!$pluginInstance instanceof iHeader)
             throw new ContainerInvalidPluginException(
                 'Invalid Plugin Provided Instance.'
             );
