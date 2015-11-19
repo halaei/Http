18c1c19e5619b522e1ef35d999942b10ed11c88f
Payam Naderi <naderi.payam@gmail.com>
2015-11-19 20:35:01 +0330

    improve headers code syntaxes (interfaces)

--- Headers.php
+++ Headers.php
@@ -5,70 +5,112 @@ use Poirot\Core\ObjectCollection;
 use Poirot\Http\Header\HeaderFactory;
 use Poirot\Http\Interfaces\iHeader;
 use Poirot\Http\Interfaces\iHeaderCollection;
+use Traversable;
 
-class Headers extends ObjectCollection
+class Headers
     implements iHeaderCollection
+    , \IteratorAggregate # implement \Traversable
 {
+    /** @var ObjectCollection */
+    protected $ObjectCollection;
+
     /**
      * Construct
+     *
+     * $headers:
+     *   ['Header-Label' => 'value, values', ..]
+     *   [iHeader, ..]
      *
      * @param array $headers
      */
     function __construct(array $headers = [])
     {
+        $this->ObjectCollection = new ObjectCollection;
+
         foreach ($headers as $label => $h) {
             if (!$h instanceof iHeader)
                 // Header-Label: value header
                 $h = HeaderFactory::factory($label, $h);
 
-            $this->attach($h);
+            $this->set($h);
         }
     }
 
     /**
-     * Attach Object
+     * Set Header
      *
-     * - replace object with new data if exists
+     * - setting the header will overwrite any
+     *   previously set header value.
      *
-     * note: recommend that object index by Unified ETag
-     *       for better search and performance
+     * ! headers label are case-insensitive
      *
-     * @param object $object
-     * @param array $data    associate array that it can be used to attach some data
-     *                       this data can be available for some codes
-     *                       block that need this data ...
-     *                       in case of render, view renderer can match
-     *                       headers that attached by itself and make
-     *                       some condition.
+     * @param iHeader $header
      *
-     * @throws \InvalidArgumentException Object Type Mismatch
-     * @return string ETag Hash Identifier of object
+     * @return $this
      */
-    function attach($object, array $data = [])
+    function set(iHeader $header)
     {
-        if (!$object instanceof iHeader)
-            throw new \InvalidArgumentException(sprintf(
-                'Invalid Header, must instance of iHeader. "%s" given.'
-                , is_object($object) ? get_class($object) : gettype($object)
-            ));
+        $search = ['label' => strtolower($header->getLabel())];
 
-        /** @var iHeader $object */
-        $data['label'] = $object->getLabel();
+        foreach($this->ObjectCollection->search($search) as $h)
+            ## previously values must overwrite
+            $this->ObjectCollection->detach($h);
 
-        return parent::attach($object, $data);
+        $this->ObjectCollection->attach($header, $search);
+
+        return $this;
     }
 
     /**
-     * @param $object
+     * Get Header With Label
      *
-     * @throws \InvalidArgumentException
+     * ! headers label are case-insensitive
+     *
+     * @param string $label Header Label
+     *
+     * @throws \Exception header not found
+     * @return iHeader
      */
-    protected function _validateObject($object)
+    function get($label)
     {
-        if (!is_object($object) && $object instanceof iHeader)
-            throw new \InvalidArgumentException(sprintf(
-                'Object must be an interface of iHeader, "%s" given.'
-                , is_object($object) ? get_class($object) : gettype($object)
-            ));
+        $r = $this->ObjectCollection->search(['label' => strtolower($label)]);
+        $r = current($r);
+
+        if (!$r instanceof iHeader)
+            throw new \Exception("Header ({$label}) not found.");
+
+        return $r;
+    }
+
+    /**
+     * Has Header With Specific Label?
+     *
+     * ! headers label are case-insensitive
+     *
+     * @param string $label
+     *
+     * @return bool
+     */
+    function has($label)
+    {
+        $r = $this->ObjectCollection->search(['label' => strtolower($label)]);
+        $r = current($r);
+
+        return (boolean) $r;
+    }
+
+
+    // Implement Traversable
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Retrieve an external iterator
+     * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
+     * @return Traversable An instance of an object implementing <b>Iterator</b> or
+     * <b>Traversable</b>
+     */
+    public function getIterator()
+    {
+        return $this->ObjectCollection;
     }
 }
--- Interfaces/iHeaderCollection.php
+++ Interfaces/iHeaderCollection.php
@@ -1,7 +1,42 @@
 <?php
 namespace Poirot\Http\Interfaces;
 
-use Poirot\Core\Interfaces\iObjectCollection;
+interface iHeaderCollection //unknown error extends \Traversable
+{
+    /**
+     * Set Header
+     *
+     * - setting the header will overwrite any
+     *   previously set header value.
+     *
+     * ! headers label are case-insensitive
+     *
+     * @param iHeader $header
+     *
+     * @return $this
+     */
+    function set(iHeader $header);
 
-interface iHeaderCollection extends iObjectCollection
-{ }
+    /**
+     * Get Header With Label
+     *
+     * ! headers label are case-insensitive
+     *
+     * @param string $label Header Label
+     *
+     * @throws \Exception header not found
+     * @return iHeader
+     */
+    function get($label);
+
+    /**
+     * Has Header With Specific Label?
+     *
+     * ! headers label are case-insensitive
+     *
+     * @param string $label
+     *
+     * @return bool
+     */
+    function has($label);
+}
