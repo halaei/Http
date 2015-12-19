372016382f2bfc3dec2c218553db5958824ca115
Payam Naderi <naderi.payam@gmail.com>
2015-12-19 17:04:41 +0330

    define request methods as consts

--- Psr/HttpRequest.php
+++ Psr/HttpRequest.php
@@ -15,6 +15,21 @@ class HttpRequest extends HttpMessage
 {
     const STREAM_CONTENT = 'php://memory';
 
+    /**#@+
+     * @const string METHOD constant names
+     */
+    const METHOD_OPTIONS  = 'OPTIONS';
+    const METHOD_GET      = 'GET';
+    const METHOD_HEAD     = 'HEAD';
+    const METHOD_POST     = 'POST';
+    const METHOD_PUT      = 'PUT';
+    const METHOD_DELETE   = 'DELETE';
+    const METHOD_TRACE    = 'TRACE';
+    const METHOD_CONNECT  = 'CONNECT';
+    const METHOD_PATCH    = 'PATCH';
+    const METHOD_PROPFIND = 'PROPFIND';
+    /**#@-*/
+
     /** @var UriInterface */
     protected $uri;
     /** @var string Request Method */
